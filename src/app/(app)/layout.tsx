fdd2bae4e9ada81d19619a11be8f2b3cd8b25293
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-07-14 19:47:09 +0000

    todas navbar devem ficar fixas

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -32,7 +32,7 @@ export default function AppLayout({ children }: { children: ReactNode }) {
       <SidebarProvider>
         <SidebarNav />
         <SidebarInset>
-            <div className="flex flex-col flex-1">
+            <div className="flex flex-col h-screen">
                 <Header />
                 <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">{children}</main>
             </div>
