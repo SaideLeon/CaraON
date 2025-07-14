cccf55fd96001aa3c41ee32819ee17ef619b4c15
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-07-14 17:44:08 +0000

    navbar nao pode ser influenciado por scrollbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { PlusCircle, Wifi, WifiOff } from 'lucide-react';
@@ -22,7 +23,7 @@ export function Header() {
 
 
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:px-6">
         <SidebarTrigger className="md:hidden"/>
         <div className="flex-1">
             <h1 className="text-xl sm:text-2xl font-headline font-bold">Instances</h1>
