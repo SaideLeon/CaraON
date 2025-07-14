3b693b6f4a54b58ad7145004069c4d4c94d9e29c
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-07-14 20:05:18 +0000

    depois de expandir o layout o botao para trazer de volta o sidebar desap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -76,8 +76,8 @@ export function Header() {
 
 
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-        <SidebarTrigger className="md:hidden"/>
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:px-6">
+        <SidebarTrigger />
         <div className="flex-1">
             <h1 className="text-xl sm:text-2xl font-headline font-bold">{title}</h1>
             <p className="text-xs sm:text-sm text-muted-foreground hidden md:block">{description}</p>
