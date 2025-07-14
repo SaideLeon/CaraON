a334896bf0381fc17d3623f1db33a0eb60d49c4f
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-07-14 06:06:20 +0000

    Status code 404

---
+++ src/next.config.ts
@@ -0,0 +1,31 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'https://caraon-backend-v2-4c5n4x5l7a-uc.a.run.app/api/v1/:path*',
+      },
+    ]
+  },
+};
+
+export default nextConfig;
