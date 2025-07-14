2fa4b299ada15667c45984929be4b9c26801cec8
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-07-14 05:56:29 +0000

    Crie service/api.ts com axios

---
+++ src/services/api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://caraonback.cognick.qzz.io/api/v1';
+const TOKEN_KEY = 'caraon-token';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    // Check if window is defined to ensure this runs only on the client-side
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem(TOKEN_KEY);
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
