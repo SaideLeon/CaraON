783ce899697fee4402372fc5156a959915976a9a
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-07-28 19:46:31 +0000

    ainda continua se movendo junto com html principal

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -35,7 +35,7 @@ export default function AppLayout({ children }: { children: ReactNode }) {
         <SidebarInset>
             <div className="flex flex-col h-screen">
                 <Header />
-                <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 bg-muted/40">{children}</main>
+                <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 bg-muted/40 min-h-0">{children}</main>
             </div>
         </SidebarInset>
       </SidebarProvider>
